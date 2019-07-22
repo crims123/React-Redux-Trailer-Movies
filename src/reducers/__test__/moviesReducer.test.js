1710952535d2a8821dd3511d5f352afa860ada77
Cristian David Pinto <cristiandavid.pinto@globant.com>
2019-07-22 09:52:48 -0500

    Delete unnecesary import

--- src/reducers/__test__/moviesReducer.test.js
+++ src/reducers/__test__/moviesReducer.test.js
@@ -1,10 +1,20 @@
 import moviesReducer from "../moviesReducer";
 import { FETCH_MOVIES_SUCCESS, FETCH_MOVIE_SUCCESS } from "../../actions/types";
-import { isTSAnyKeyword, exportAllDeclaration, arrowFunctionExpression } from "@babel/types";
+
 
 describe("moviesReducer", () => {
     it("send a FETCH_MOVIES_SUCCESS action type", () => {
         const action = { type: FETCH_MOVIES_SUCCESS, payload: "data"}
-        expect(moviesReducer(action)).toEqual({popularMovies: action.payload})
+        expect(moviesReducer([], action)).toEqual({popularMovies: action.payload})
+    })
+
+    it("send a FETCH_MOVIE_SUCCESS action type", () => {
+        const action = { type: FETCH_MOVIE_SUCCESS, payload: {name: "Movie Name", id:21}}
+        expect(moviesReducer([], action)).toEqual({movieDetails: payload, movieSelectedId:payload.id})
+    })
+
+    it("send an action with diferrent type", () => {
+        const action = { type: "NO_NAME" }
+        expect(moviesReducer([], action)).toEqual([])
     })
 })
