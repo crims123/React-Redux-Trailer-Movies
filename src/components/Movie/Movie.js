80a53f174695a87b5651f37bdb583d6262e98a90
Cristian David Pinto <cristianpinto@natgeo.com>
2019-11-28 14:11:12 -0500

    Fixed undefined trailers

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -9,65 +9,65 @@ const Movie = props => {
     }
   } = props;
 
-  const movieId =
-    movieDetails &&
-    movieDetails.videos &&
-    movieDetails.videos.results !== 0 &&
-    movieDetails.videos.results[0].key;
-
-  if (props.movie.movies.length === 0) {
+  if (!props.movie.movies.movieDetails) {
     return null;
-  }
-
-  return (
-    <div className="movie">
-      <div className="container-fluid">
-        <div className="movie__container">
-          <div className="movie__cover-box">
-            <img
-              src={`
+  } else {
+    return (
+      <div className="movie">
+        <div className="container-fluid">
+          <div className="movie__container">
+            <div className="movie__cover-box">
+              <img
+                src={`
                 https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movieDetails &&
                   movieDetails.poster_path}
               `}
-              alt="movie-cover"
-              title="cover-movie"
-              className="movie__cover"
-            />
-          </div>
-
-          <div className="movie__info">
-            <p className="movie__info-average">
-              User Score: {movieDetails && movieDetails.vote_average}
-            </p>
-            <h4 className="movie__info-title">
-              {movieDetails && movieDetails.title}
-            </h4>
-            <h3>Overview</h3>
-            <p className="movie__info-overview">
-              {movieDetails && movieDetails.overview}
-            </p>
-          </div>
-        </div>
-        {movieId ? (
-          <div className="movie__trailer-container">
-            <div className="movie__trailer">
-              <iframe
-                title="trailer"
-                width="100%"
-                height="100%"
-                src={`https://www.youtube.com/embed/${movieId}?controls=1`}
-                frameBorder="0"
-                allow="allowfullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen="true"
+                alt="movie-cover"
+                title="cover-movie"
+                className="movie__cover"
               />
             </div>
+
+            <div className="movie__info">
+              <p className="movie__info-average">
+                User Score: {movieDetails && movieDetails.vote_average}
+              </p>
+              <h4 className="movie__info-title">
+                {movieDetails && movieDetails.title}
+              </h4>
+              <h3>Overview</h3>
+              <p className="movie__info-overview">
+                {movieDetails && movieDetails.overview}
+              </p>
+            </div>
           </div>
-        ) : (
-          <h2>Sorry we don't have a trailer for this Movie</h2>
-        )}
+          {movieDetails.videos.results.length !== 0 ? (
+            <div className="movie__trailer-container">
+              <div className="movie__trailer">
+                <iframe
+                  title="trailer"
+                  width="100%"
+                  height="100%"
+                  src={`https://www.youtube.com/embed/${
+                    movieDetails.videos.results[0].key
+                  }?controls=1`}
+                  frameBorder="0"
+                  allow="allowfullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="movie__not-found">
+              <h2 className="movie__not-found-text">
+                Sorry we don't have a trailer for this Movie
+              </h2>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Movie;
