9009ec62574ee357572ea810e12e79e83dd8c8b0
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2019-12-05 14:55:34 -0500

    Added home page to Header

--- src/shared/layout/Header.js
+++ src/shared/layout/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Header.scss";
 
 const Header = () => {
-  return <header className="header">Developed by Cristian David Pinto</header>;
+  return (
+    <Link className="header__link" to="/page/1">
+      <header className="header">Developed by Cristian David Pinto</header>
+    </Link>
+  );
 };
 
 export default Header;
